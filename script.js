e361c09103634fdf4d4f26f9b079ecabf296a93b
Lara <debbiegterra@gmail.com>
2021-09-01 15:15:12 +0100

    JavvaScript code completed

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
-const panel = document.querySelectorAll('.panel');
+const panels = document.querySelectorAll('.panel');
 
-// Function to increase the flex basis
+//Function to increase the flex basis
 function toggleOpen() {
-    
+    this.classList.toggle('open');
 }
+
+//Function to make the text pop in
+function toggleActive(e) {
+    if(e.propertyName.includes('flex')) {
+        this.classList.add('open-active');
+    }
+}
+
+panels.forEach(panel => panel.addEventListener('click', toggleOpen));
+panels.forEach(panel => panel.addEventListener('transitionend', toggleActive));
